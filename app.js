088083134ce7dc8d9782ab4797aa573ae88c6c90
prathmesh2102003 <prathmeshamale646@gmail.com>
2024-09-19 23:48:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end("WELCOME TO THE FORTUNE CLOUD!!!\n'");
+    res.end("This CI/CD pipeline (using node.js) deployed by Prathmesh using GitHub_Action!!!\n'");
 });
 server.listen(port, () => {
     console.log(`Server running on port ${port}/`);
